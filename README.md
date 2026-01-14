8feaa4ba601ff376285d9ddbae2ff2f4825cc765
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-15 03:02:09 +0700

    Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+```bash
+
 @reboot sleep 60 && cd /home/zulfikriyahya/bot-visitor && ./setup.sh > bot.log 2>&1
 
-0 _/6 _ \* \* pkill -f main.py; pkill -f setup.sh; cd /home/zulfikriyahya/bot-visitor && ./setup.sh > bot.log 2>&1
+0 */6 * * * pkill -f main.py; pkill -f setup.sh; cd /home/zulfikriyahya/bot-visitor && ./setup.sh > bot.log 2>&1
+```
--- setup.sh
+++ setup.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-# --- KONFIGURASI NAMA FILE ---
+
 PROXY_SCRIPT="proxy.py"
 CHECKER_SCRIPT="checker.py"
 MAIN_BOT_SCRIPT="main.py"
 ENV_DIR="env"
 
-# Fungsi untuk menangani Ctrl+C (Exit Gracefully)
+
 trap "echo -e '\n[STOP] Script dihentikan user.'; exit" SIGINT
 
 echo "==========================================="
 echo "   AUTO TRAFFIC BOT SETUP (DEBIAN)       "
 echo "==========================================="
 
-# 1. Cek & Buat Virtual Environment
+
 if [ ! -d "$ENV_DIR" ]; then
     echo "[INFO] Membuat Virtual Environment ($ENV_DIR)..."
     python3 -m venv $ENV_DIR
 fi
 
-# 2. Aktifkan Environment
+
 echo "[INFO] Mengaktifkan Environment..."
 source $ENV_DIR/bin/activate
 
-# 3. Cek & Install Dependencies
+
 echo "[INFO] Cek & Install Library Python..."
 pip install --upgrade pip > /dev/null
-# Install library tanpa output berisik (-q)
+
 pip install selenium pandas webdriver-manager requests -q
 
-# 4. LOOP UTAMA (OTOMATISASI)
+
 while true; do
     echo ""
     echo "#############################################"
     echo "   MEMULAI SIKLUS BARU (Update -> Check -> Run)"
     echo "#############################################"
     
-    # --- STEP A: DOWNLOAD PROXY ---
+    
     if [ -f "$PROXY_SCRIPT" ]; then
         echo "[STEP 1] Menjalankan $PROXY_SCRIPT..."
         /home/zulfikriyahya/bot-visitor/env/bin/python3 $PROXY_SCRIPT
@@ -47,7 +47,7 @@ while true; do
     
     echo "---------------------------------------------"
 
-    # --- STEP B: CHECK PROXY ---
+    
     if [ -f "$CHECKER_SCRIPT" ]; then
         echo "[STEP 2] Menjalankan $CHECKER_SCRIPT..."
         /home/zulfikriyahya/bot-visitor/env/bin/python3 $CHECKER_SCRIPT
@@ -58,16 +58,16 @@ while true; do
 
     echo "---------------------------------------------"
 
-    # --- STEP C: JALANKAN BOT UTAMA ---
+    
     if [ -f "$MAIN_BOT_SCRIPT" ]; then
         echo "[STEP 3] Menjalankan $MAIN_BOT_SCRIPT..."
         echo "Bot sedang berjalan. Tekan Ctrl+C untuk berhenti."
         
-        # Jalankan bot
+        
         /home/zulfikriyahya/bot-visitor/env/bin/python3 $MAIN_BOT_SCRIPT
         
-        # Jika bot script utama Anda crash atau selesai (misal karena error fatal),
-        # script ini akan lanjut ke bawah.
+        
+        
         echo ""
         echo "[WARNING] Bot utama berhenti/crash."
     else
